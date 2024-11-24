61ed4d48f0e2ded5a46cb75d66fd2102caddbdfa
alpha <alfaali308@gmail.com>
2024-11-24 10:56:03 +0000

    feat: Add `BackgroundRemovalEnginesSchema` schema.& fix: Correct `thumbnail_url` URLs for `engines`.

--- app/apps/background_removal/schemas.py
+++ app/apps/background_removal/schemas.py
@@ -30,11 +30,25 @@ class BackgroundRemovalEngines(str, Enum):
 
     @property
     def thumbnail_url(self):
-        return "https://cdn.metisai.com/images/engines/{}.png".format(self.value)
+        return {
+            BackgroundRemovalEngines.cjwbw: "https://media.pixiee.io/v1/f/c3044ffe-8fb5-410c-b5e2-3939a9140266/cjwbw-icon.png",
+            BackgroundRemovalEngines.lucataco: "https://media.pixiee.io/v1/f/2a6a6b6d-45ac-486b-861d-e00477d52ac4/lucataco-icon.png",
+            BackgroundRemovalEngines.pollinations: "https://media.pixiee.io/v1/f/aa6f73c6-ab73-48bf-8b96-1a83aa238a1c/pollinations-icon.png",
+        }[self]
 
     @property
     def price(self):
         return 0.1
+
+
+class BackgroundRemovalEnginesSchema(BaseModel):
+    engine: BackgroundRemovalEngines = BackgroundRemovalEngines.cjwbw
+    thumbnail_url: str
+    price: float
+
+    @classmethod
+    def from_model(cls, model: BackgroundRemovalEngines):
+        return cls(engine=model, thumbnail_url=model.thumbnail_url, price=model.price)
 
 
 class BackgroundRemovalCreateSchema(BaseModel):
