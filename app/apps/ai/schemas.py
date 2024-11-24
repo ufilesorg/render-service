0c8a40c0b886f69e64ec8612977ee72db69f82e7
alpha <alfaali308@gmail.com>
2024-11-24 10:55:30 +0000

    fix: Correct `thumbnail_url` URLs for `engines`.

--- app/apps/ai/schemas.py
+++ app/apps/ai/schemas.py
@@ -61,8 +61,8 @@ class ImaginationEngines(str, Enum):
     flux_schnell = "flux_schnell"
     stability = "stability"
     flux_1_1 = "flux_1.1"
-    # flux = "flux"
     dalle = "dalle"
+    # flux = "flux"
     # leonardo = "leonardo"
 
     @property
@@ -91,12 +91,12 @@ class ImaginationEngines(str, Enum):
     @property
     def thumbnail_url(self):
         return {
-            ImaginationEngines.dalle: "",
-            ImaginationEngines.midjourney: "",
-            ImaginationEngines.ideogram: "",
-            ImaginationEngines.flux_schnell: "",
-            ImaginationEngines.stability: "",
-            ImaginationEngines.flux_1_1: "",
+            ImaginationEngines.dalle: "https://media.pixiee.io/v1/f/41af8b03-b4df-4b2f-ba52-ea638d10b5f3/dalle-icon.png",
+            ImaginationEngines.midjourney: "https://media.pixiee.io/v1/f/4a0980aa-8d97-4493-bdb1-fb3d67d891e3/midjourney-icon.png",
+            ImaginationEngines.ideogram: "https://media.pixiee.io/v1/f/19d4df43-ea1e-4562-a8e1-8ee301bd0a88/ideogram-icon.png",
+            ImaginationEngines.flux_schnell: "https://media.pixiee.io/v1/f/cf21c500-6e84-4915-a5d1-19b8f325a382/flux-icon.png",
+            ImaginationEngines.stability: "https://media.pixiee.io/v1/f/6d0a2e82-7667-46ec-af33-0e557f16e356/stability-icon.png",
+            ImaginationEngines.flux_1_1: "https://media.pixiee.io/v1/f/cf21c500-6e84-4915-a5d1-19b8f325a382/flux-icon.png",
         }[self]
 
     @property
